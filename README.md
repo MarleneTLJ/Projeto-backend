a43365da54b986f78d4ba06c219c7e85f85b9f17
Marlene Tonel <marlene.lopes@grupoprominas.com.br>
2022-04-12 13:39:29 -0300

    Adição de um README e inserção de confirmação de senha na hora de cadastrar um usuário

---
+++ README.md
@@ -0,0 +1,11 @@
+## Pré-requisitos
+
+- Node.js (de preferência a versão LTS) - [Página de download](https://nodejs.org/en/download/)
+- MongoDB - [Página de download](https://www.mongodb.com/download-center/community)
+
+## Instalação
+
+```
+npm install
+npm start
+```
--- validation.js
+++ validation.js
@@ -1,34 +1,23 @@
-const Joi = require('@hapi/joi');
+const Joi = require("@hapi/joi");
 
 // Validação de registro
 const registerValidation = (data) => {
-    const schema = Joi.object ({
-        name: Joi.string()
-            .min(3)
-            .required(),
-        email: Joi.string()
-            .min(6)
-            .required()
-            .email(),
-        password: Joi.string()
-            .min(6)
-            .required()
-    });
-    return schema.validate(data);
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+    repeatPassword: Joi.string().required().valid(Joi.ref('password'))
+  });
+  return schema.validate(data);
 };
 
 // Validação de login
 const loginValidation = (data) => {
-    const schema = Joi.object ({
-        email: Joi.string()
-            .min(6)
-            .required()
-            .email(),
-        password: Joi.string()
-            .min(6)
-            .required()
-    });
-    return schema.validate(data);
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
 };
 
 module.exports.registerValidation = registerValidation;
