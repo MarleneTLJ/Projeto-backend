9d05de2489f24d354b1c6c908811314a5fcd8b2c
Marlene Tonel <marlene.tonel@lytex.com.br>
2022-04-25 09:01:51 -0300

    Limpando o código

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -15,18 +15,3 @@ function generateToken(user) {
 
   return jwt.sign(token, config.tknSecret);
 }
-
-// module.exports = function (req, res, next) {
-//   const token = req.header('auth-token');
-//   if (!token) {
-//     return res.status(401).send('Acesso Negado!');
-//   }
-
-//   try {
-//     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-//     req.user = verified;
-//     next();
-//   } catch (err) {
-//     res.status(400).send('Token inválido!');
-//   }
-// }
